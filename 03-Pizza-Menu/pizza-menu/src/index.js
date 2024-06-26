88a9b1c1e27c317d1b884bf522b919c1f15a54a4
epochnt <ntt45419@gmail.com>
2024-06-26 23:56:18 +0530

    Added all components and some basic styling

--- 03-Pizza-Menu/pizza-menu/src/index.js
+++ 03-Pizza-Menu/pizza-menu/src/index.js
@@ -1,8 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        imgPath="pizzas/spinaci.jpg"
+        price={10}
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  // const style = {  color: "red" };
+  return (
+    <div className="pizza">
+      <img src={props.imgPath} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  const date = new Date();
+  const time = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+  const openingHour = 12;
+  const closingHour = 22;
+  const isOpen = date.getHours() > openingHour && date.getHours() < closingHour;
+  console.log(isOpen);
+
+  return <footer className="footer">{time} We're currently open</footer>;
 }
 
 // React v18
@@ -14,4 +68,4 @@ root.render(
 );
 
 // React before 18
-// React.render(<App />);
+// React.render(<App />, document.getElementById("root") );
